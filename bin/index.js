7cf001614ebd7165ce837de947e9384564d55aa2
Celso de Almeida Leite Filho <celso.almeida.leite@hotmail.com>
2020-08-24 19:15:46 -0300

    Edição das Views do projeto front-end.

--- bin/index.js
+++ bin/index.js
@@ -12,15 +12,54 @@ app.set('view engine','handlebars')
 app.use(express.static(path.join(__dirname,'../public')))
 
 // rotas
-// app.get('/',(red, res) => { res.sendFile(
-//     path.join(__dirname,'../pages/index.html'))
-// })
+app.get('/', (req, res) => {
+  res.render('home/index',
+  {
+    title: 'Inicio',
+    cover_title: 'Mighty Web Design Agency',
+    cover_text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis
+    in tenetur doloremque, maiores doloribus officia iste. Dolores.`
+  })
+})
 
-app.get('/', (req, res) => { res.render('home/index')})
-app.get('/about', (req, res) => { res.render('home/about')})
-app.get('/contact', (req, res) => { res.render('home/contact')})
-app.get('/journal', (req, res) => { res.render('home/journal')})
-app.get('/work', (req, res) => { res.render('home/work')})
+app.get('/about', (req, res) => {
+  res.render('home/about',{
+    title: 'Sobre',
+    cover_title: 'Sobre',
+    cover_text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis
+    in tenetur doloremque, maiores doloribus officia iste. Dolores.`
+  })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('home/contact',
+  {
+    title: 'Contato',
+    cover_title: 'Contato',
+    cover_text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis
+    in tenetur doloremque, maiores doloribus officia iste. Dolores.`
+  })
+})
+
+app.get('/journal', (req, res) => {
+  res.render('home/journal',
+  {
+    title: 'Blog',
+    cover_title: 'Blog',
+    cover_text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis
+    in tenetur doloremque, maiores doloribus officia iste. Dolores.`
+  })
+})
+
+app.get('/work', (req, res) => {
+  res.render('home/work',
+  {
+    title: 'Serviços',
+    cover_title: 'Serviços',
+    cover_text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis
+    in tenetur doloremque, maiores doloribus officia iste. Dolores.`
+  })
+})
 
 // outros
 app.listen(port, () => { console.log(`server runnin is http://localhost:${port}`) })
